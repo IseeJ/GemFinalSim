0a6e108677f3bf76964821a62c702d70e6495dc3
IseeJ <122057875+IseeJ@users.noreply.github.com>
2023-11-05 23:16:34 -0500

    Update avalanche2.cc

--- avalanche2.cc
+++ avalanche2.cc
@@ -70,7 +70,7 @@ int main(int argc, char* argv[]) {
   sensor->AddComponent(elm);
   
   // sensor->SetArea(-axis_x, -axis_y, -axis_z, axis_x, axis_y, axis_z);
-  sensor->SetArea(-5 * pitch, -5 * pitch, -0.01, 5 * pitch,  5 * pitch,  0.025);
+  sensor->SetArea(-5 * pitch, -5 * lem_pitch, -0.01, 5 * lem_pitch,  5 * lem_pitch,  0.025);
   sensor->AddElectrode(elm, "wtlel");
   // Set the signal binning.
   const double tEnd = 500.0;
@@ -81,8 +81,6 @@ int main(int argc, char* argv[]) {
   AvalancheMicroscopic* aval = new AvalancheMicroscopic();
   aval->SetSensor(sensor);
   aval->SetCollisionSteps(100);
-  //added
-  aval->SetDistanceSteps(2.e-4);
 
   // Set up the object for drift line visualization.
   ViewDrift* viewDrift = new ViewDrift();
